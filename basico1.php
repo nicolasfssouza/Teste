ecbd3e3f315b53557ca2d6bd2b30fc99bbdd956f
nicolasfssouza <nicolasfssouza@hotmail.com>
2016-06-15 19:24:15 -0400

    teste de envio online

---
+++ basico1.php
@@ -0,0 +1,44 @@
+<?php
+//OPERADORES
+
+//concatenar
+/* $name = "nicolas";
+$last_name = "ferreira";
+
+echo $name." ".$last_name;
+*/
+/*soma
+$x = 10;
+$y = 8;
+$x++;
+$y+=3;
+echo $x + $y;
+*/
+/*subtraçao
+$x = 10;
+$y = 8;
+$x --;
+$y -=3;
+echo $x - $y;
+*/
+/*divisão
+$x = 10;
+$y = 8;
+echo $x / $y;
+*/
+/*multiplicação
+$x = 10;
+$y = 8;
+echo $x * $y;
+*/
+//exponencial - introduzido no php 5.6
+$x = 2;
+$y = 2;
+//echo $x** $y;
+
+//módulo
+//echo 4%33;
+
+echo (4 + 4) /4;//com o parenteses vc define qual operação será feita primeiro. Seguindo a logica matematica.
+
+?>
